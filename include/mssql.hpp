ad543cba4902a280810e4e0956aa6025a1952ed1
Everett Vergara <evergara@shinra.com.ph>
2022-08-25 20:14:20 +0800

    added mssql class

--- include/mssql.hpp
+++ include/mssql.hpp
@@ -11,3 +11,21 @@ typedef struct STR_BINDING {
 } BINDING;
 
 
+void HandleDiagnosticRecord (SQLHANDLE hHandle, SQLSMALLINT hType, RETCODE RetCode);
+void DisplayResults(HSTMT hStmt, SQLSMALLINT cCols);
+void AllocateBindings(HSTMT hStmt, SQLSMALLINT cCols, BINDING** ppBinding, SQLSMALLINT*  pDisplay);
+
+
+namespace g80 {
+    class mssql {
+
+    private:
+        SQLHENV hEnv = NULL;
+        SQLHDBC hDbc = NULL;
+        SQLHSTMT hStmt = NULL;
+        WCHAR* pwszConnStr;
+        WCHAR wszInput[SQL_QUERY_SIZE];
+
+
+    };
+}
