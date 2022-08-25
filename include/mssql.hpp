65de63f0c03d1653e637212d2e19075f470d3156
Everett Vergara <evergara@shinra.com.ph>
2022-08-25 22:55:08 +0800

    misc bug corrections

--- include/mssql.hpp
+++ include/mssql.hpp
@@ -8,8 +8,6 @@
 namespace g80 {
     namespace odbc {
 
-        void HandleDiagnosticRecord(SQLHANDLE hHandle, SQLSMALLINT hType, RETCODE RetCode);
-
         class odbc {
 
         private:
@@ -17,6 +15,29 @@ namespace g80 {
             SQLHDBC         hDbc_{nullptr};
             SQLHSTMT        hStmt_{nullptr};
             std::wstring    last_error_{};
+
+            void check_for_errors(SQLHANDLE hHandle, SQLSMALLINT hType, RETCODE rc) {
+                
+                SQLSMALLINT iRec = 0;
+                SQLINTEGER  iError;
+                WCHAR       wszMessage[1000];
+                WCHAR       wszState[SQL_SQLSTATE_SIZE+1];
+
+                if (rc == SQL_INVALID_HANDLE) {
+                    last_error_ = L"Invalid Handle";
+                    return;
+                }
+
+                while (SQLGetDiagRec(hType, hHandle, ++iRec, wszState, &iError, wszMessage,
+                        (SQLSMALLINT)(sizeof(wszMessage) / sizeof(WCHAR)),
+                        (SQLSMALLINT *)NULL) == SQL_SUCCESS) {
+                    
+                    if (wcsncmp(wszState, L"01004", 5))
+                    {
+                        fwprintf(stderr, L"[%5.5s] %s (%d)\n", wszState, wszMessage, iError);
+                    }
+                }
+            }
 
         public:
 
@@ -31,23 +52,23 @@ namespace g80 {
                 return last_error_;
             }
 
-            auto handle_ret_code(RETCODE rc) -> bool {
-                if(rc != SQL_SUCCESS) HandleDiagnosticRecord(hEnv_, SQL_HANDLE_ENV, rc);
+            auto handle_ret_code(SQLHANDLE handle, SQLSMALLINT type, RETCODE rc) -> bool {
+                if(rc != SQL_SUCCESS) check_for_errors(handle, type, rc);
                 if(rc == SQL_ERROR) return false;
                 return true;
             }
 
             auto alloc_env() -> bool {
                 if(SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &hEnv_) == SQL_ERROR) return false;
-                return handle_ret_code(SQLSetEnvAttr(hEnv_, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0)); 
+                return handle_ret_code(hEnv_, SQL_HANDLE_ENV, SQLSetEnvAttr(hEnv_, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0)); 
             }
 
-            auto alloc_handle() -> bool {
-                return handle_ret_code(SQLAllocHandle(SQL_HANDLE_DBC, hEnv_, &hDbc_));
+            auto alloc_connection() -> bool {
+                return handle_ret_code(hEnv_, SQL_HANDLE_ENV, SQLAllocHandle(SQL_HANDLE_DBC, hEnv_, &hDbc_));
             }
 
             auto connect_by_dsn(const std::wstring &server, const std::wstring &user, const std::wstring &passwd) -> bool {
-                return handle_ret_code(SQLConnect(hDbc_, 
+                return handle_ret_code(hDbc_, SQL_HANDLE_DBC, SQLConnect(hDbc_, 
                     const_cast<wchar_t *>(server.c_str()), server.size(), 
                     const_cast<wchar_t *>(user.c_str()), user.size(), 
                     const_cast<wchar_t *>(passwd.c_str()), passwd.size()));
@@ -57,7 +78,7 @@ namespace g80 {
                 wchar_t buff[1024];
                 SQLSMALLINT actualsize;
                 std::wstring conn_str = L"FILEDSN=D:\\Everett\\Codes\\Projects\\Personal\\Tools\\MSSQL-Connector\\db\\local.dsn; UID=sa; PWD=Kerberos2014!";
-                return handle_ret_code(SQLDriverConnect(hDbc_, NULL, 
+                return handle_ret_code(hDbc_, SQL_HANDLE_DBC, SQLDriverConnect(hDbc_, NULL, 
                     const_cast<wchar_t *>(conn_str.c_str()),
                     conn_str.size(), 
                     buff, 1024, &actualsize, SQL_DRIVER_NOPROMPT));
@@ -79,36 +100,6 @@ namespace g80 {
         };
 
 
-        void HandleDiagnosticRecord (SQLHANDLE      hHandle,
-                                    SQLSMALLINT    hType,
-                                    RETCODE        RetCode)
-        {
-            SQLSMALLINT iRec = 0;
-            SQLINTEGER  iError;
-            WCHAR       wszMessage[1000];
-            WCHAR       wszState[SQL_SQLSTATE_SIZE+1];
 
-            if (RetCode == SQL_INVALID_HANDLE)
-            {
-                fwprintf(stderr, L"Invalid handle!\n");
-                return;
-            }
-
-            while (SQLGetDiagRec(hType,
-                                hHandle,
-                                ++iRec,
-                                wszState,
-                                &iError,
-                                wszMessage,
-                                (SQLSMALLINT)(sizeof(wszMessage) / sizeof(WCHAR)),
-                                (SQLSMALLINT *)NULL) == SQL_SUCCESS)
-            {
-                // Hide data truncated..
-                if (wcsncmp(wszState, L"01004", 5))
-                {
-                    fwprintf(stderr, L"[%5.5s] %s (%d)\n", wszState, wszMessage, iError);
-                }
-            }
-        }
     }
 }
