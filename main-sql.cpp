993125be4d6ad742680ca986b790f912fce9decc
Everett Vergara <evergara@shinra.com.ph>
2022-08-27 15:47:26 +0800

    misc edits

--- main-sql.cpp
+++ main-sql.cpp
@@ -7,8 +7,8 @@ auto main(int argc, const char *argv[]) -> int {
 
     odbc sqlca;
 
-    if(!sqlca.connect_by_dsn(L"test", L"sa", L"Kerberos2014!")) 
-        std::wcout << "ERROR";
+    if(!sqlca.connect_by_dsn(L"test", L"sa", L"Kerberos2014!x")) 
+        std::wcout << sqlca.get_formatted_last_error();
     
     // if(!sqlca.disconnect())
     //     std::wcout << sqlca.get_formatted_last_error();
