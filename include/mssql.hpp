f3f3215b0c0d2e47e620b89064fe48dccd824dff
Everett Vergara <evergara@shinra.com.ph>
2022-08-27 15:25:46 +0800

    misc edits

--- include/mssql.hpp
+++ include/mssql.hpp
@@ -29,21 +29,18 @@ namespace g80 {
             SQLHSTMT    stmt_{NULL};
             odbc_error_mgr  err_;
 
-            void check_for_message(SQLHANDLE hHandle, SQLSMALLINT hType, RETCODE rc) {
+            void check_for_message(SQLHANDLE handle, SQLSMALLINT type, RETCODE rc) {
                 if(rc == SQL_INVALID_HANDLE) {
                     set_user_error(L"Invalid Handle");
                     return;
                 } 
-                // SQLSMALLINT i = 0;
-            //     do {
-            //         // auto &m = msg_.emplace_back();
+                SQLSMALLINT i = 0;
+                do {
+                    auto m = err_.get_next_slot();
+                    RETCODE rc = SQLGetDiagRec(type, handle, ++i, m->last_state, &m->last_error, m->last_message, ERROR_MESSAGE_SIZE, static_cast<SQLSMALLINT *>(NULL));
+                    if(rc != SQL_SUCCESS) {m.pop_back(); break;}                
 
-            //         // RETCODE rc = SQLGetDiagRec(hType, hHandle, ++i, m.last_state, &m.last_error, m.last_message, 
-            //         //                 MESSAGE_SIZE, static_cast<SQLSMALLINT *>(NULL));
-                    
-            //         // if(rc != SQL_SUCCESS) {msg_.pop_back(); break;}                
-
-            //    } while(true);
+               } while(true);
             }
 
             auto set_user_error(const WCHAR *error_msg) -> bool {
