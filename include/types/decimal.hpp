8b14dfb0d7ada956874537395c6f1e8c821e2e21
Everett Vergara <evergara@shinra.com.ph>
2022-08-28 23:00:23 +0800

    added constructors

--- include/types/decimal.hpp
+++ include/types/decimal.hpp
@@ -8,12 +8,17 @@ namespace g80 {
         class decimal {
 
         private:
-            int64_t data_;
+            int64_t data_{0};
             uint8_t scale_;
 
         public:
-            decimal(uint8_t scale) : scale_{scale} {}
-            
+            decimal(uint8_t scale = 2) : scale_{scale} {}
+            ~decimal() = default;
+            decimal(const decimal &r) : data_{r.data_}, scale_{r.scale_} {}
+            decimal(decimal &&r) : data_{r.data_}, scale_{r.scale_} {}
+            auto operator=(const decimal &r) -> decimal & {data_ = {r.data_}, scale_ = {r.scale_}; return *this;}
+            auto operator=(decimal &&r) -> decimal & {data_ = {r.data_}, scale_ = {r.scale_}; return *this;}
+
         };
     }
 }
