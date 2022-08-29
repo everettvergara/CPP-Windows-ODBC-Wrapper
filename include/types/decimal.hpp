a67e2fd00e5ab7d82c05f0857866d7ed4745c0d1
Everett Vergara <evergara@shinra.com.ph>
2022-08-29 11:32:04 +0800

    misc edits

--- include/types/decimal.hpp
+++ include/types/decimal.hpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <algorithm>
 #include <cstdint>
-
+#include <type_traits>
 
 namespace g80 {
     namespace odbc {
@@ -12,8 +12,12 @@ namespace g80 {
         private:
             int64_t data_{0};
             int8_t scale_;
+            int64_t scale_mul_;
 
-
+        auto inline scale_mul(int8_t s) -> int64_t {
+        
+        }
+        
         auto inline data_on_scale(int8_t s) -> int64_t {
             int8_t ds = s - scale_;
             if(ds == 0) return data_;
@@ -27,24 +31,24 @@ namespace g80 {
         }
 
         public:
+
             decimal(int8_t scale = 2) : scale_{scale} {}
             decimal(const decimal &r) : data_{r.data_}, scale_{r.scale_} {}
             decimal(decimal &&r) : data_{r.data_}, scale_{r.scale_} {}
             ~decimal() = default;
             auto operator=(const decimal &r) -> decimal & {data_ = {r.data_}, scale_ = {r.scale_}; return *this;}
             auto operator=(decimal &&r) -> decimal & {data_ = {r.data_}, scale_ = {r.scale_}; return *this;}
+            template<typename T> requires std::is_integral<T>::value auto operator=(const T r) -> decimal & {data_ = r;}
+            template<typename T> requires std::is_floating_point<T>::value auto operator=(const T r) -> decimal & {
+                // data_ = static_cast<int64_t>(r * ;
+
+                return *this;
+            }
+
 
             auto operator+=(decimal r) -> decimal & {
-
-                // 1234.56000
-                //   12.34567
                 auto gs = std::max(scale_, r.scale_);
                 data_ = this->data_on_scale(gs) + r.data_on_scale(gs);
-
-                // 1234.5678
-                //   12.34
-
-
                 return *this;
             }
         };
